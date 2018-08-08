fb3aed6e1be3dcb094f9082954185df2cba1e3be
dcamriff <driffelatl@gmail.com>
2018-08-08 17:00:22 -0400

    imported Order and Inventory components to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from './Header'
+import Order from './Order'
+import Inventory from './Inventory'
 
 class App extends React.Component {
     render() {
         return (
             <div className="catch-of-the-day">
-                <Header></Header>
+                <div className="menu">
+                <Header />
+                </div>
+                <Order />
+                <Inventory />
             </div>
         )
     }
 }
+
+export default App;
