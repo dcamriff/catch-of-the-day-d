8c91da33a76523d5ae1f98c8fbfc61308bcb946f
dcamriff <driffelatl@gmail.com>
2018-08-08 17:14:59 -0400

    added span and props to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 class Header extends React.Component {
     render() {
+        console.log(this)
         return (
             <header className="top">
+                <h1>
+                    Catch
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    Day
+                    </span>
+                </h1>
+                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
                 
             </header>
         )
