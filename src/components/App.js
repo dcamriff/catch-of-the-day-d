5720e1cd8e973273b8338ba1159d826178b9e92b
dcamriff <driffelatl@gmail.com>
2018-08-08 16:48:21 -0400

    slight syntax revisions to get caught up

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 class App extends React.Component {
     render() {
         return (
             <div className="catch-of-the-day">
-                <Header></Header></Header>
-            </div></div>
+                <Header></Header>
+            </div>
         )
     }
 }
