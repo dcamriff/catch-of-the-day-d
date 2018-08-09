0a4140735231553b7032ff5e99d098ca568be7d7
dcamriff <driffelatl@gmail.com>
2018-08-09 11:22:47 -0400

    added NotFound component and updated index with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
-import StorePicker from './components/StorePicker'
-import App from './components/App'
+
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
 import './css/style.css'
+import App from './components/App'
+
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Route exactly pattern="/" component={StorePicker} />
+                <Route pattern="/store/:storeId" component={App} />
+                <Switch component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
 
 render (<App />, document.querySelector('#main'));
